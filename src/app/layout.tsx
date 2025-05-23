bb7eacaa804dc2ed2e52bc87feb675a75c20eef3
khoivu <khoi.vu@nextchain.kr>
2025-05-23 10:21:26 +0700

    feat: update home ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import {NuqsAdapter} from 'nuqs/adapters/next/app'
 import {ThemeProvider} from 'next-themes'
 import Sidebar from '@/components/Sidebar/Sidebar'
 import Header from '@/components/Header/Header'
+import SearchDataInput from '@/components/SearchDataInput/SearchDataInput'
 
 const firaCode = Fira_Code({
 	variable: '--font-fira-code',
@@ -40,10 +41,13 @@ export default async function RootLayout({
 								enableSystem
 								disableTransitionOnChange
 							>
-								<main className='container mx-auto desktop:flex'>
+								<main className='relative container mx-auto desktop:flex'>
 									<Sidebar />
 									<Header />
-									<div className='mt-[61px] desktop:mt-0 desktop:pt-[80px] w-full desktop:w-[70%] py-[10px] px-[10px] tablet:px-[30px] h-dvh desktop:border-x border-neutral-700 border-dashed overflow-y-auto'>
+									<div className=' relative mt-[61px] desktop:mt-0 desktop:pt-[80px] w-full desktop:w-[70%] py-[10px] px-[10px] tablet:px-[30px] h-dvh desktop:border-x border-neutral-700 border-dashed overflow-y-auto'>
+										<div className='absolute top-[20px] left-1/2 -translate-x-1/2 w-full h-full'>
+											<SearchDataInput />
+										</div>
 										{children}
 									</div>
 								</main>
